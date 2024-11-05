c21b7388eb7ae79450ef29e884de7ecec4b1c86a
Yabeth <soteloyabeth@gmail.com>
2024-11-04 18:33:21 -0600

    metodo cambiar estado

--- app_java_project/app_java_project/src/app_java_project/ControlCondominio.java
+++ app_java_project/app_java_project/src/app_java_project/ControlCondominio.java
@@ -1,35 +1,51 @@
 package app_java_project;
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author samue
  */
 public class ControlCondominio {
-    
-    private ArrayList <DatosQuisckpass> ArrayQuis;
-    private ArrayList <DatosQuisckpass> ArrayQuisEliminados;
-    
-    public ControlCondominio(){
-    this.ArrayQuis = new ArrayList<>();
+
+    private ArrayList<DatosQuisckpass> ArrayQuis;
+    private ArrayList<DatosQuisckpass> ArrayQuisEliminados;
+
+    public ControlCondominio() {
+        this.ArrayQuis = new ArrayList<>();
+        this.ArrayQuisEliminados = new ArrayList<>();
     }
-    
-    public void AgregarQuis(DatosQuisckpass a){
+
+    public void AgregarQuis(DatosQuisckpass a) {
         this.ArrayQuis.add(a);
-        
     }
-    
-    public int CantQuis(){
-    return this.ArrayQuis.size();
+
+    public int CantQuis() {
+        return this.ArrayQuis.size();
     }
-    
-    public DatosQuisckpass MostrarQuis(){
-    DatosQuisckpass b = null;
+
+    public DatosQuisckpass MostrarQuis() {
+        DatosQuisckpass b = null;
         for (DatosQuisckpass dato : ArrayQuis) {
             b = dato;
         }
-    return b;
+        return b;
     }
-    
+
+    public boolean CambiarEstado(String codigo) {
+        for (DatosQuisckpass dato : ArrayQuis) {
+            if (dato.getCodigo().equals(codigo)) {
+                // Cambiar el estado del Quisckpass al contrario del actual
+                if (dato.getEstado() == Estado.Activo) {
+                    dato.setEstado(Estado.Inactivo);
+                } else {
+                    dato.setEstado(Estado.Activo);
+                }
+                return true; // Cambio exitoso
+            }
+        }
+        return false; // No se encontró el código
+    }
+
 }
