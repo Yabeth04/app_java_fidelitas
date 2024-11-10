779d959e7983cffb38a9656cbd9ca5ff2b595842
Yabeth <soteloyabeth@gmail.com>
2024-11-10 17:33:47 -0600

    delete code innecesary

--- app_java_project/app_java_project/src/app_java_project/App_java_project.java
+++ app_java_project/app_java_project/src/app_java_project/App_java_project.java
@@ -21,129 +21,13 @@ public class App_java_project {
 
             switch (opcionMenuPrincipal) {
                 case 1 -> {
-                    //FALTA ARREGLAR EL SETPLACA 
-                    //HAY QUE PONERLE RESTRICCIONES
-                    condomino.setFilial();
-
-                    condomino.setCodigo();
-
-                    condomino.setPlaca();
-
-                    condomino.AgregarQuis();
-
+                    //AGREGAR
                 }
                 case 2 -> {
-                    int opcionMenuConsultar = 0;
-                    int opcionSubMenuConsultar = 0;
-
-                    opcionMenuConsultar = Integer.parseInt(JOptionPane.showInputDialog(
-                            "********* MENU CONSULTAR *********\n"
-                            + "1) Visualizar Datos\n2) Visualizar Datos Eliminados\n0) Salir"));
-
-                    switch (opcionMenuConsultar) {
-                        case 1 -> {
-                            opcionSubMenuConsultar = Integer.parseInt(JOptionPane.showInputDialog(
-                                    "********* VISUALIZAR DATOS *********\n"
-                                    + "1) Todos los datos \n2) Los de una filial \n3) Uno en especifico \n0) Salir"));
-                            String consultaNumeroFilial = "";
-                            switch (opcionSubMenuConsultar) {
-                                case 1 -> {
-                                    //MOSTRAR TODOS LOS DATOS
-                                    JOptionPane.showMessageDialog(null, condomino.MostrarQuis());
-                                }
-                                case 2 -> {
-                                    //MOSTRAR LOS DATOS DE UNA FILIAL
-                                    consultaNumeroFilial = JOptionPane.showInputDialog(null, "Ingrese el número de la filial");
-                                    JOptionPane.showMessageDialog(null, condomino.MostrarFilial(consultaNumeroFilial));
-
-                                }
-                                case 3 -> {
-                                    //MOSTRAR LOS DATOS DE UNO EN ESPECIFICO
-                                    consultaNumeroFilial = JOptionPane.showInputDialog(
-                                            null, "Ingrese el número de la filial o el código");
-                                    String resultadoBusqueda = condomino.MostrarUnoEspecifico(consultaNumeroFilial);
-                                    JOptionPane.showMessageDialog(null, resultadoBusqueda);
-                                }
-                                case 0 -> {
-                                    //SALIR
-                                }
-                                default ->
-                                    JOptionPane.showMessageDialog(null, "ERROR: Opcion no valida");
-                            }
-
-                        }
-                        case 2 -> {
-                            //VISUALIZAR DATOS ELIMINADOS
-                            opcionSubMenuConsultar = Integer.parseInt(JOptionPane.showInputDialog(
-                                    "********* VISUALIZAR DATOS ELIMINADOS *********\n"
-                                    + "1) Todos los datos \n2) Los de una filial \n3) Uno en especifico \n0) Salir"));
-
-                            switch (opcionSubMenuConsultar) {
-                                case 1 -> {
-                                    //MOSTRAR TODOS LOS DATOS
-
-                                }
-                                case 2 -> {
-                                    //MOSTRAR LOD DATOS DE UNA FILIAL
-
-                                }
-                                case 3 -> {
-                                    //MOSTRAR LOS DATOS DE UNO EN ESPECIFICO
-
-                                }
-                                case 0 -> {
-                                    break;
-                                }
-                                default ->
-                                    JOptionPane.showMessageDialog(null, "ERROR: Opcion no valida");
-                            }
-                        }
-                        case 0 -> {
-
-                        }
-                        default ->
-                            JOptionPane.showMessageDialog(null, "ERROR: Opcion no valida");
-                    }
-
-                    //EN LAS DOS FORMAS DEBE BUSCAR PRIMERO QUE SI EXISTA EL OBJETO
-                    //CREAR METODOS
+                    //CONSULTAR
                 }
                 case 3 -> {
                     //ELIMINAR
-
-                    int opcionMenuEliminar = Integer.parseInt(JOptionPane.showInputDialog(
-                            "****** MENU ELIMINAR ******\n1) Eliminar Placa\n2)Eliminar Codigo\n3) Salir"));
-                    switch (opcionMenuEliminar) {
-                        case 1 -> {
-                            // Eliminar placa
-                            String placaEliminar = JOptionPane.showInputDialog(null, "Ingrese la placa a eliminar");
-                            boolean eliminado = condomino.EliminarPorPlaca(placaEliminar);
-                            if (eliminado) {
-                                JOptionPane.showMessageDialog(null, "Placa eliminada con éxito.");
-                            } else {
-                                JOptionPane.showMessageDialog(null, "Placa no encontrada.");
-                            }
-                        }
-                        case 2 -> {
-                            // Eliminar placa
-                            String placaEliminar = JOptionPane.showInputDialog(null, "Ingrese el codigo a eliminar");
-                            boolean eliminado = condomino.EliminarPorCodigo(placaEliminar);
-                            if (eliminado) {
-                                JOptionPane.showMessageDialog(null, "Codigo eliminada con éxito.");
-                            } else {
-                                JOptionPane.showMessageDialog(null, "Codigo no encontrado.");
-                            }
-                        }
-                        case 3 -> {
-                            //SALIR
-                        }
-                        default ->
-                            JOptionPane.showMessageDialog(null, "ERROR: Opcion no valida");
-                    }
-                    //HACER UN SUBMENU PARA LAS DOS FORMAS DE ELIMINAR
-                    //METODO ELIMINAR CON LA PLACA
-                    //METODO ELIMINAR CON EL CODIGO
-                    //DEBEN MOVERSE A OTRO ARRAY
                 }
                 case 4 -> {
                     //CAMBIAR ESTADO
@@ -154,7 +38,7 @@ public class App_java_project {
                     if (cambioExitoso) {
                         JOptionPane.showMessageDialog(null, "Estado cambiado exitosamente.");
                     } else {
-                        JOptionPane.showMessageDialog(null, "No se encontró el Quisckpass con ese código.");
+                        JOptionPane.showMessageDialog(null, "Codigo no valido o no existe.");
                     }
                 }
                 case 5 -> {
--- app_java_project/app_java_project/src/app_java_project/DatosQuisckpass.java
+++ app_java_project/app_java_project/src/app_java_project/DatosQuisckpass.java
@@ -2,257 +2,32 @@ package app_java_project;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author samue
- */
 public class DatosQuisckpass {
 
-    int Size = 10;
-    private String ArrayEliminados[][] = new String[Size][4];
-    private String ArrayQuis[][] = new String[Size][4];
+    private String codigo = "1000000001"; // Ejemplo de código inicial
+    private Estado estado = Estado.Activo; // Estado inicial
 
-    private String filial = null;
-    private String codigo = null;
-    private String placa = null;
-    private Estado estado = Estado.Activo;
-
+    // Constructor vacío
     public DatosQuisckpass() {
     }
 
-    public DatosQuisckpass(String nulo) {
-        this.setFilial();
-        this.setCodigo();
-        this.setPlaca();
-        this.getEstado();
-    }
-
-    public String getFilial() {
-        return filial;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public String getPlaca() {
-        return placa;
-    }
-
+    // Obtener el estado actual
     public Estado getEstado() {
         return estado;
     }
 
-    //CAMBIAR ESTADO
+    // Obtener el código actual
+    public String getCodigo() {
+        return codigo;
+    }
+
     public boolean CambiarEstado(String pCodigo) {
-        for (int i = 0; i < ArrayQuis.length; i++) {
-            for (int j = 0; j < ArrayQuis[i].length; j++) {
-                if (pCodigo.equals(ArrayQuis[i][j])) {
-                    if (ArrayQuis[i][3].equals("" + Estado.Activo)) {
-                        ArrayQuis[i][3] = "" + Estado.Inactivo;
-                    } else {
-                        ArrayQuis[i][3] = "" + Estado.Activo;
-                    }
-                    return true;
-                }
-            }
+        if (pCodigo.length() == 10 && pCodigo.substring(0, 3).equals("101") && pCodigo.equals(codigo)) {
+
+            estado = (estado == Estado.Activo) ? Estado.Inactivo : Estado.Activo;
+            return true; // Cambio exitoso
         }
-        return false;
-    }
-
-    public void setFilial() {
-        boolean bandera = true;
-
-        while (bandera) {
-            String pFilial = JOptionPane.showInputDialog(null, "Ingrese el número del Filial");
-            if (pFilial.length() > 0) {
-                filial = pFilial;
-                bandera = false;
-            } else {
-                JOptionPane.showMessageDialog(null, "Error del tamaño del Filial");
-            }
-        }
-    }
-
-    public void setCodigo() {
-        boolean bandera = true;
-
-        while (bandera) {
-            String pCodigo = JOptionPane.showInputDialog(null, "Ingrese el número del codigo");
-            if (pCodigo.startsWith("101")) {
-                if (pCodigo.length() == 10) {
-                    codigo = pCodigo;
-                    bandera = false;
-
-                } else {
-                    JOptionPane.showMessageDialog(null, "Error del tamaño del Codigo");
-                }
-            } else {
-                JOptionPane.showMessageDialog(null, "Error el codigo no comienza en 101");
-            }
-        }
-    }
-
-    public void setPlaca() {
-        boolean bandera = true;
-
-        while (bandera) {
-            String pPlaca = JOptionPane.showInputDialog(null, "Ingrese el número de la placa");
-            if (pPlaca.length() > 0) {
-                placa = pPlaca;
-                bandera = false;
-            } else {
-                JOptionPane.showMessageDialog(null, "Error del tamaño de la placa");
-            }
-        }
-    }
-
-    // AGREGAR / INGRESAR CODIGO,FILIAL Y PLACA
-    public void AgregarQuis() {
-        int limite = 0;
-
-        for (int i = 0; i < ArrayQuis.length; i++) {
-            for (int j = 0; j < ArrayQuis[i].length; j++) {
-                if (ArrayQuis[i][j] == null && limite < 4) {
-                    switch (j) {
-                        case 0 -> {
-                            ArrayQuis[i][j] = filial;
-                        }
-                        case 1 -> {
-                            ArrayQuis[i][j] = codigo;
-                        }
-                        case 2 -> {
-                            ArrayQuis[i][j] = placa;
-                        }
-                        case 3 -> {
-                            ArrayQuis[i][j] = "" + estado;
-                        }
-                    }
-                    limite += 1;
-                }
-            }
-        }
-    }
-
-    // OPCION CONSULTAR
-    //TODOS LOS DATOS
-    public String MostrarQuis() {
-        String Res = "";
-        for (int i = 0; i < ArrayQuis.length; i++) {
-            for (int j = 0; j < ArrayQuis[i].length; j++) {
-                if (ArrayQuis[i][j] != null) {
-                    switch (j) {
-                        case 0 -> {
-                            Res += "Filial: " + ArrayQuis[i][j];
-                        }
-                        case 1 -> {
-                            Res += ", Codigo: " + ArrayQuis[i][j];
-                        }
-                        case 2 -> {
-                            Res += ", Placa: " + ArrayQuis[i][j];
-                        }
-                        case 3 -> {
-                            Res += ", Estado: " + ArrayQuis[i][j] + "\n";
-                        }
-                    }
-                }
-            }
-        }
-        return Res;
-    }
-
-    //LOS DE UNA FILIAL
-    public String MostrarFilial(String pConsulta) {
-        String Res = "";
-        for (int i = 0; i < ArrayQuis.length; i++) {
-            for (int j = 0; j < ArrayQuis[i].length; j++) {
-                if (ArrayQuis[i][j] != null) {
-                    if (pConsulta.equals(ArrayQuis[i][j])) {
-                        Res += "Filial: " + ArrayQuis[i][0];
-                        Res += ", Codigo: " + ArrayQuis[i][1];
-                        Res += ", Placa: " + ArrayQuis[i][2];
-                        Res += ", Estado: " + ArrayQuis[i][3] + "\n";
-                    }
-                }
-            }
-        }
-        return Res;
-    }
-
-    //MOSTRAR UNO EN ESPECIFICO
-    public String MostrarUnoEspecifico(String pConsulta) {
-        String Res = "";
-        for (int i = 0; i < ArrayQuis.length; i++) {
-            for (int j = 0; j < ArrayQuis[i].length; j++) {
-                if (ArrayQuis[i][j] != null) {
-                    // Buscar por código (o placa, depende de la entrada)
-                    if (pConsulta.equals(ArrayQuis[i][1])) {  // 1 es el índice para el código
-                        Res += "Filial: " + ArrayQuis[i][0];
-                        Res += ", Codigo: " + ArrayQuis[i][1];
-                        Res += ", Placa: " + ArrayQuis[i][2];
-                        Res += ", Estado: " + ArrayQuis[i][3] + "\n";
-                        return Res; // Devolver solo el primer resultado encontrado
-                    }
-                }
-            }
-        }
-        return "No se encontró el código/placa especificado.";
-    }
-
-    //ELIMINAR 
-    //ELIMINAR PLACA 
-    public boolean EliminarPorPlaca(String placa) {
-        for (int i = 0; i < ArrayQuis.length; i++) {
-            if (ArrayQuis[i][2] != null && ArrayQuis[i][2].equals(placa)) {
-                // busqueda de la placa, se "copia" a ArrayEliminados
-                for (int j = 0; j < ArrayEliminados.length; j++) {
-                    // se busca la primera fila vacía en ArrayEliminados para agregarse
-                    if (ArrayEliminados[j][0] == null) {
-                        ArrayEliminados[j][0] = ArrayQuis[i][0];
-                        ArrayEliminados[j][1] = ArrayQuis[i][1];
-                        ArrayEliminados[j][2] = ArrayQuis[i][2];
-                        ArrayEliminados[j][3] = ArrayQuis[i][3];
-                        break;  // se termina de "copiar" y sale del loop
-                    }
-                }
-
-                //  elimina la placa de arrayquis
-                ArrayQuis[i][0] = null;
-                ArrayQuis[i][1] = null;
-                ArrayQuis[i][2] = null;
-                ArrayQuis[i][3] = null;
-
-                return true;  // La placa eliminada-
-            }
-        }
-        return false;  // La placa existe
-    }
-
-    // ELIMINAR POR CÓDIGO
-    public boolean EliminarPorCodigo(String codigo) {
-        for (int i = 0; i < ArrayQuis.length; i++) {
-            if (ArrayQuis[i][1] != null && ArrayQuis[i][1].equals(codigo)) {
-                // busqueda del código, se "copia" a ArrayEliminados
-                for (int j = 0; j < ArrayEliminados.length; j++) {
-                    // se busca la primera fila vacía en ArrayEliminados para agregarse
-                    if (ArrayEliminados[j][0] == null) {
-                        ArrayEliminados[j][0] = ArrayQuis[i][0];
-                        ArrayEliminados[j][1] = ArrayQuis[i][1];
-                        ArrayEliminados[j][2] = ArrayQuis[i][2];
-                        ArrayEliminados[j][3] = ArrayQuis[i][3];
-                        break;  // se termina de "copiar" y sale del loop
-                    }
-                }
-
-                // elimina el código de ArrayQuis
-                ArrayQuis[i][0] = null;
-                ArrayQuis[i][1] = null;
-                ArrayQuis[i][2] = null;
-                ArrayQuis[i][3] = null;
-
-                return true;  // el coodigo eliminado
-            }
-        }
-        return false; //la placa si existe
+        return false; // No se encontró el código
     }
 }
+
